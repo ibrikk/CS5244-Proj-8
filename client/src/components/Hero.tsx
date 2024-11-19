149b49a8fc20d859ac01f2b5c45f64051a8b284c
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-11-19 17:22:26 -0500

    work on validation

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ShoppingBag } from "lucide-react";
 import "../assets/css/Home.css";
 
@@ -8,19 +8,22 @@ export interface HeroProps {
 }
 
 const Hero: React.FC<HeroProps> = ({ firstCategory }) => {
+  const navigate = useNavigate();
   return (
     <div className="homepage grid grid-full">
       <section className="grid-full hero-section">
         <div className="hero-content">
           <h1>Discover pages of unknown stories and wisdom.</h1>
         </div>
-        <Link
-          to={`/category/${firstCategory}`.replace(" ", "%20")}
+        <button
           className="hero-button"
+          onClick={() =>
+            navigate(`/category/${firstCategory}`.replace(" ", "%20"))
+          }
         >
           <ShoppingBag className="pr-5" />
           <p className="cta-text">Start Shopping</p>
-        </Link>
+        </button>
       </section>
     </div>
   );
